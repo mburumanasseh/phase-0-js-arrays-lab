18f2bbd0dbb60a3c79c6e62a391055345b3b2f04
mburumanasseh <manassehmugo714@gmail.com>
2026-03-23 10:15:39 +0300

    Add inventoryManagement.js file

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,20 @@
-// Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(productName) {
+  products.push(productName);
+}
+
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+
+function removeLastProduct() {
+  products.pop();
+}
 
 
 // Export the necessary parts for testing
